7d8b06e6bb47faad9a86aff3f6378a8568a4954b
codeuser-crypto <vijayshreyank@gmail.com>
2025-01-22 13:29:00 +0530

    committed

---
+++ second_class.py
@@ -0,0 +1,30 @@
+print("Albert Einstein once said")
+print('Albert Einstein once said')
+
+print("*")
+print("**")
+print("***")
+print("****")
+
+print("Addition : 5+3 = ", 5+3)
+print("Addition : 5-3 = ", 5-3)
+print("Addition : 5*3 = ", 5*3)
+print("Addition : 5/3 = ", 5/3)
+
+print("Using \ for new line")
+print("Using \\ for new line")
+print("First Line\nSecond Line")
+print("\nUsing \\t for a tab")
+print("Column1\tColumn2\nColumn3")
+print("\nusing \\\* for quotes")
+print("He said, \"Python is awesome!\"")
+
+print("Name\tAge\tCity")
+print("Alice\t25\tLondon")
+print("Bob\t30\tNYC")
+
+print("Hello", "World", end = '###')
+print("Bye", "World", sep = '$$')
+
+message = "GOOD DAY"
+print(message)
