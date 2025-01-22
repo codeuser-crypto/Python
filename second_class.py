59eeff7cd037cd043cc713cbef7ced68966d52dd
codeuser-crypto <vijayshreyank@gmail.com>
2025-01-22 13:43:28 +0530

    committed

--- second_class.py
+++ second_class.py
@@ -28,3 +28,37 @@ print("Bye", "World", sep = '$$')
 
 message = "GOOD DAY"
 print(message)
+
+#1. Variable names can contain only letters, numbers, and underscores.
+#2.⁠ ⁠They can start with a letter or an underscore, but not with a number.
+#3.⁠ ⁠Spaces are not allowed in variable names, but underscores can be used to separate word
+#4.⁠ ⁠Avoid using Python keywords and function names as variable Names
+#5.⁠ ⁠Variable names should be short but descriptive.
+# For example, name is better than n, student_name is better than s_n
+#6. Be careful when using the lowercase letter l and the uppercase letter O because they
+#7.⁠ ⁠reserved words (35)cannot be used as variable names and, as, assert,
+# break, class, continue, def, del, elif, else, except, False
+# if, import, in, is, lambda, None, nonlocal, not, or, pass
+
+msg = "GOOD DAY"
+print(msg.title())
+print(msg.lower())
+print(msg.upper())
+print(msg.swapcase())
+
+msg2 = "  GOOD  "
+print(msg2.strip())
+print(msg2.lstrip())
+print(msg2.rstrip())
+
+a = 1000
+b = 1_0_00
+print(a)
+print(b)
+
+a = "rose"
+b = "beautiful"
+c = f'{a} is a {b} flower'
+print(c)
+c = f'{a.title()} is a {b} flower'
+print(c)
