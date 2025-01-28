a6cf94ae805448709fc9b686a16590ef8624094b
codeuser-crypto <vijayshreyank@gmail.com>
2025-01-28 13:38:11 +0530

    committed

--- third_class.py
+++ third_class.py
@@ -8,4 +8,29 @@ print(list[1:3])
 print(list[:4])
 print(list[4:])
 print(list[-4:-1])
-print(list[-1])  
+print(list[-1])
+
+list[1] = 'Z'
+print(list)
+list[1:3] = ['X', 'Y']
+print(list)
+
+list1 = ['A', 'B', 'C']
+list2 = ['X', 'Y', 'Z']
+list1.extend(list2)
+print(list1)
+
+del list1[1]
+print(list1)
+
+list1 = ['A', 'B', 'C']
+tuple = ['X', 'Y', 'Z']
+list1.extend(tuple)
+print(list1)
+
+list1.remove('X')
+print(list)
+del list1[0]
+print(list1)
+list1.clear()
+print(list1)
