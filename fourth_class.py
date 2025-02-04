96902cbff1106897d979344f9c8f557f90bb6f8b
codeuser-crypto <vijayshreyank@gmail.com>
2025-02-04 14:02:47 +0530

    committed

--- fourth_class.py
+++ fourth_class.py
@@ -21,3 +21,29 @@ celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = celsius_to_fahrenheit(celsius)
 
 print(f"{celsius}°C is equal to {fahrenheit:.2f}°F")
+
+student = {"name" : "alice", "age" : 25, "grade" : "A"}
+print(student["name"])
+print(student["age"])
+student["major"] = "physics"
+print(student)
+del student["grade"]
+print(student)
+for key, value in student.items():
+    print(f"{key} : {value}")
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+var = 7
+while var > 0:
+    print("Current variable value:", var)   
+    var = var-1
+    if var == 3:
+        break
+    else:
+        if var == 5:
+            var = var - 1
+            continue
+    print("Good Work")
