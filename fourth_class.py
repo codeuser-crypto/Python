d9b9f3fa0476f92c2c548f9599ca9c41f4f61759
codeuser-crypto <vijayshreyank@gmail.com>
2025-02-04 12:59:43 +0530

    committed

---
+++ fourth_class.py
@@ -0,0 +1,23 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+
+
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = celsius_to_fahrenheit(celsius)
+
+print(f"{celsius}°C is equal to {fahrenheit:.2f}°F")
